2722e77d0012559c255b9abc5a571a636c274072
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 00:43:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   if array.length === 1
     array.join
+  end
   elsif array.length === 2
     array.join(" and")
+  end
   elsif array.length === 3
     array.join
+  end
 end
